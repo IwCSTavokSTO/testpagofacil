cb66a1ed277eb95d689ae2f60837ec963299d9b8
IwCSTavokSTO <carlosmovglez@gmail.com>
2020-08-17 10:05:37 -0500

    vaciar formulario

--- resources/views/pagofacil.blade.php
+++ resources/views/pagofacil.blade.php
@@ -45,45 +45,45 @@
                     </div>
                     <div class="form-group">
                       <label for="nombre">Nombre:</label>
-                      <input class="form-control" placeholder="Nombre" id="nombre" name="nombre" value="Jon">
+                      <input class="form-control" placeholder="Nombre" id="nombre" name="nombre" value="">
                     </div>
                     <div class="form-group">
                       <label for="apellidos">Apellidos:</label>
-                      <input class="form-control" placeholder="Apellidos" id="apellidos" name="apellidos" value="Snow">
+                      <input class="form-control" placeholder="Apellidos" id="apellidos" name="apellidos" value="">
                     </div>
                     <div class="form-group">
                       <label for="numeroTarjeta">numeroTarjeta:</label>
-                      <input type="text" id="tarjeta_uno" name="tarjeta_uno" size="4" maxlength="4" value="5513">
-                      <input type="text" id="tarjeta_dos" name="tarjeta_dos" size="4" maxlength="4" value="5509">
-                      <input type="text" id="tarjeta_tres" name="tarjeta_tres" size="4" maxlength="4" value="9409">
-                      <input type="text" id="tarjeta_cuatro" name="tarjeta_cuatro" size="4" maxlength="4" value="2123">
+                      <input type="text" id="tarjeta_uno" name="tarjeta_uno" size="4" maxlength="4" value="">
+                      <input type="text" id="tarjeta_dos" name="tarjeta_dos" size="4" maxlength="4" value="">
+                      <input type="text" id="tarjeta_tres" name="tarjeta_tres" size="4" maxlength="4" value="">
+                      <input type="text" id="tarjeta_cuatro" name="tarjeta_cuatro" size="4" maxlength="4" value="">
                     </div>
                     <div class="form-group">
                       <label for="cvt">CVT:</label>
-                      <input type="text" class="form-control" id="cvt" name="cvt" size="4" maxlength="4" value="271">
+                      <input type="text" class="form-control" id="cvt" name="cvt" size="4" maxlength="4" value="">
                     </div>
                     <div class="form-group">
                       <label for="cp">Código Postal:</label>
-                      <input type="text" class="form-control" placeholder="Código Postal" id="cp" name="cp" size="5" maxlength="5" value="48219">
+                      <input type="text" class="form-control" placeholder="Código Postal" id="cp" name="cp" size="5" maxlength="5" value="">
                     </div>
                     <div class="form-group">
                       <div for="mesExpiracion">Fecha de Expiración:</div>
                       <label for="mesExpiracion">Mes:</label>
-                      <input type="text" id="mesExpiracion" name="mesExpiracion" size="2" maxlength="2" value="08">
+                      <input type="text" id="mesExpiracion" name="mesExpiracion" size="2" maxlength="2" value="">
                       <label for="anyoExpiracion">Año:</label>
-                      <input type="text" id="anyoExpiracion" name="anyoExpiracion" size="4" maxlength="4" value="22">
+                      <input type="text" id="anyoExpiracion" name="anyoExpiracion" size="4" maxlength="4" value="">
                     </div>
                     <div class="form-group">
                       <label for="monto">Monto:</label>
-                      <input type="text" class="form-control" placeholder="Monto" id="monto" name="monto" value="23456.78">
+                      <input type="text" class="form-control" placeholder="Monto" id="monto" name="monto" value="">
                     </div>
                     <div class="form-group">
                       <label for="idSucursal">Sucursal:</label>
-                      <input type="text" class="form-control" placeholder="Sucursal" id="idSucursal" name="idSucursal" value="e147ee31531d815e2308d6d6d39929ab599deb98">
+                      <input type="text" class="form-control" placeholder="Sucursal" id="idSucursal" name="idSucursal" value="">
                     </div>
                     <div class="form-group">
                       <label for="idUsuario">ID de Empresa:</label>
-                      <input type="text" class="form-control" placeholder="ID de Empresa" id="idUsuario" name="idUsuario" value="f541b3f11f0f9b3fb33499684f22f6d711f2af58">
+                      <input type="text" class="form-control" placeholder="ID de Empresa" id="idUsuario" name="idUsuario" value="">
                     </div>
                     <div class="form-group">
                         <label for="idServicio">Servicio PagoFácil:</label>
@@ -95,39 +95,39 @@
                     </div>
                     <div class="form-group">
                       <label for="email">Email:</label>
-                      <input type="email" class="form-control" placeholder="Email" id="email" name="email" value="pruebas@pagofacil.net">
+                      <input type="email" class="form-control" placeholder="Email" id="email" name="email" value="">
                     </div>
                     <div class="form-group">
                       <label for="telefono">Teléfono:</label>
-                      <input type="text" class="form-control" placeholder="Teléfono" id="telefono" name="telefono" value="5544443333">
+                      <input type="text" class="form-control" placeholder="Teléfono" id="telefono" name="telefono" value="">
                     </div>
                     <div class="form-group">
                       <label for="celular">Celular:</label>
-                      <input type="text" class="form-control" placeholder="Celular" id="celular" name="celular" value="5588889999">
+                      <input type="text" class="form-control" placeholder="Celular" id="celular" name="celular" value="">
                     </div>
                     <div class="form-group">
                       <label for="calleyNumero">Calle y número:</label>
-                      <input type="text" class="form-control" placeholder="" id="calleyNumero" name="calleyNumero" value="Calle No 7">
+                      <input type="text" class="form-control" placeholder="" id="calleyNumero" name="calleyNumero" value="">
                     </div>
                     <div class="form-group">
                       <label for="colonia">Colonia:</label>
-                      <input type="text" class="form-control" placeholder="" id="colonia" name="colonia" value="Mi colonia">
+                      <input type="text" class="form-control" placeholder="" id="colonia" name="colonia" value="">
                     </div>
                     <div class="form-group">
                       <label for="municipio">Municipio:</label>
-                      <input type="text" class="form-control" placeholder="" id="municipio" name="municipio" value="Mi Mun">
+                      <input type="text" class="form-control" placeholder="" id="municipio" name="municipio" value="">
                     </div>
                     <div class="form-group">
                       <label for="estado">Estado:</label>
-                      <input type="text" class="form-control" placeholder="" id="estado" name="estado" value="Mi estado">
+                      <input type="text" class="form-control" placeholder="" id="estado" name="estado" value="">
                     </div>
                     <div class="form-group">
                       <label for="pais">Pais:</label>
-                      <input type="text" class="form-control" placeholder="" id="pais" name="pais" value="México">
+                      <input type="text" class="form-control" placeholder="" id="pais" name="pais" value="">
                     </div>
                     <div class="form-group">
                       <label for="idPedido">ID de pedido:</label>
-                      <input type="text" class="form-control" placeholder="ID de pedido" id="idPedido" name="idPedido" value="4567">
+                      <input type="text" class="form-control" placeholder="ID de pedido" id="idPedido" name="idPedido" value="">
                     </div>
 
                     <button class="btn btn-primary" id="submit-button">Realizar transacción</button>
